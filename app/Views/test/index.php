9243198a5f4a51dd9cf14b63ba638d5b41bd2eea
KattiaBibi <kattia1997-@hotmail.com>
2022-06-20 09:27:07 -0500

    camb

--- app/Views/test/index.php
+++ app/Views/test/index.php
@@ -17,13 +17,55 @@
     iframe {
       border-radius: 10px;
       height: 700px;
-      width: 300px;
+      width: 500px;
     }
+
+
+    .contenedor {
+    margin: 2rem auto;
+    border: 1px solid #aaa;
+    height: 300px;
+    width:90%;
+    max-width: 400px;
+    background: #f1f2f3;
+    overflow:auto;
+    box-sizing: border-box;
+    padding:0 1rem;
+}
+
+/* Estilos para motores Webkit y blink (Chrome, Safari, Opera... )*/
+
+.contenedor::-webkit-scrollbar {
+    -webkit-appearance: none;
+}
+
+.contenedor::-webkit-scrollbar:vertical {
+    width:10px;
+}
+
+.contenedor::-webkit-scrollbar-button:increment,.contenedor::-webkit-scrollbar-button {
+    display: none;
+} 
+
+.contenedor::-webkit-scrollbar:horizontal {
+    height: 10px;
+}
+
+.contenedor::-webkit-scrollbar-thumb {
+    background-color: #797979;
+    border-radius: 20px;
+    border: 10px solid #f1f2f3;
+}
+
+.contenedor::-webkit-scrollbar-track {
+    border-radius: 10px;  
+}
+
   </style>
 </head>
 
-<body>
-  <iframe src="http://tarjetaspot.local/compusistel/bibiana-cruzado" frameborder="0"></iframe>
+<body>        
+  <iframe src="http://tarjetaspot.local/compusistel/bibiana-cruzado" frameborder="0" class="contenedor"></iframe>
 </body>
 
 </html>
