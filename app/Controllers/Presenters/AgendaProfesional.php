c609d56bac9a9fb762b23bf1d5e8e949a1c3e443
davidmc1996 <elbooz30@hotmail.com>
2022-07-01 18:02:42 -0500

    Update AgendaProfesional.php

--- app/Controllers/Presenters/AgendaProfesional.php
+++ app/Controllers/Presenters/AgendaProfesional.php
@@ -3,18 +3,31 @@
 namespace App\Controllers\Presenters;
 
 use App\Controllers\BaseController;
+use App\Libraries\GeneradorVCard;
 
 class AgendaProfesional extends BaseController
 {
-	public function index($slugEmpresa = null)
+	public function index($slug = null)
 	{
 		helper('datos_tarjeta_helper');
-		$agendaProfesional = model('UsuarioModel')->getForAgendaProfesional($slugEmpresa);
+		$agendaProfesional = model('UsuarioModel')->getForAgendaProfesional($slug);
 
-		if (!$agendaProfesional['empresa']) {
-			throw new \CodeIgniter\Exceptions\PageNotFoundException("No se encontro la agenda profesional para la empresa ($slugEmpresa)");
+    $usuario = model('DatosUsuarioModel')->where('slug', $slug)->first();
+
+		if ($agendaProfesional['empresa']) {
+			return view('agenda-profesional/index', ['agenda_profesional' => $agendaProfesional]);
+		} 
+
+		if ($usuario) {
+			$datosUsuario = model('UsuarioModel')->getForTarjetaDigital($usuario['id_usuario']);
+
+			$generadorVCard = new GeneradorVCard();
+			$qrVcardImg = $generadorVCard->generarVcard($datosUsuario, 'QR');
+	
+			return view('tarjeta_digital/default', ['d' => $datosUsuario, 'qrVcardImg' => $qrVcardImg]);
 		}
 
-		return view('agenda-profesional/index', ['agenda_profesional' => $agendaProfesional]);
+		throw new \CodeIgniter\Exceptions\PageNotFoundException("No se encontro la agenda profesional para la empresa ó usuario ($slug)");
+
 	}
 }
